183a983d80d45e95caedd6bfa7ce5c57adb452d1
accors <61780738+accors@users.noreply.github.com>
2022-10-03 10:12:59 +0800

    Change default ip to avoid conflict

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Add date version
 export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
@@ -18,9 +18,9 @@ sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\
 popd
 
 # Rename hostname to OpenWrt
-pushd package/base-files/files/bin
-sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
-popd
+# pushd package/base-files/files/bin
+# sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
+# popd
 
 # Change default shell to zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
