c0324befe125e11542a542b0a79dde95240f3602
accors <61780738+accors@users.noreply.github.com>
2022-10-11 19:30:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Add luci-app-mosdns
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
