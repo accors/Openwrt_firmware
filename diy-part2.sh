62fba98a8e022f673dcfc516af04e7809bdee4ef
accors <61780738+accors@users.noreply.github.com>
2023-02-15 16:22:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,3 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # Add date version
 export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
 sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
-
-# Rename hostname
-pushd package/base-files/files/bin
-sed -i 's/ImmortalWrt/FastWrt/g' config_generate
-popd
